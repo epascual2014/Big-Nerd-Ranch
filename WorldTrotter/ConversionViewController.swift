2ac5c51271a12046b3a359a950a96a62f615a01a
Edrick Pascual <edrickpascual@Edricks-MacBook-Pro.local>
2016-02-24 16:40:03 -0800

    NumberFormatter Added

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -10,13 +10,84 @@ import UIKit
 
 class ConversionViewController: UIViewController {
     
+    @IBOutlet var textField: UITextField?
     
     @IBOutlet var celsiusLabel: UILabel!
     
-    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
+    var fahrenheitValue: Double? {
         
-        celsiusLabel.text = textField.text
+        didSet {
+            
+            updateCelsiusLabel()
+            
+        }
         
     }
+    
+    
+    
+    var celsiusValue: Double? {
+        
+        if let value = fahrenheitValue {
+            
+            return (value - 32) * (5/9)
+            
+        } else {
+            
+            return nil
+            
+        }
+        
+        
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
+        
+        if let text = textField.text, value = Double(text) {
+            
+            fahrenheitValue = value
+            
+        } else {
+            
+            fahrenheitValue = nil
+            
+        }
+        
+    }
+    
+    
+    func updateCelsiusLabel() {
+        
+        if let value = celsiusValue {
+            
+            celsiusLabel.text = "\(value)"
+        
+        } else {
+        
+            celsiusLabel.text = "answer"
+            
+        }
+        
+    }
+    
+    
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        
+        textField!.resignFirstResponder()
+    }
+    
+    
+    //Number Formatter
+    
+    let numberFormatter: NSNumberFormatter = {
+        
+        let nf = NSNumberFormatter()
+        nf.numberStyle = .DecimalStyle
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+        
+        
+    } ()
     
 }

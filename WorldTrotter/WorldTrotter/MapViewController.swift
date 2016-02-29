cdea92efdd854557d16e2fa8e27cc731066dc03b
Edrick Pascual <edrickpascual@Edricks-MacBook-Pro.local>
2016-02-29 14:17:44 -0800

    MapView created via code
    MapView Created with code and constraints added.

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -6,16 +6,45 @@
 //  Copyright © 2016 Edrick Pascual. All rights reserved.
 //
 
-import Foundation
+import MapKit
 import UIKit
 
 class MapViewController: UIViewController {
     
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        
+        //MapView Created
+        mapView = MKMapView()
+        
+        //Set the view to the view controller
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+            segmentedControl.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
+            segmentedControl.selectedSegmentIndex = 0
+            segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(segmentedControl)
+        
+        let topContstraint = segmentedControl.topAnchor.constraintEqualToAnchor(view.leadingAnchor)
+        let leadingConstraint = segmentedControl.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor)
+
+        topContstraint.active = true
+        leadingConstraint.active = true
+        trailingConstraint.active = true
+    
+    }
+    
+    
+    
+    //var currentTime: NSTimeZone
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("MapView Controller Loaded its view")
-        
+        print("MVC loaded")
         
     }
     

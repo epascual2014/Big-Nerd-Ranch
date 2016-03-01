89cde8c9e305f17a46f62b0403904f4287f35e20
Edrick Pascual <edrickpascual@Edricks-MacBook-Pro.local>
2016-02-29 20:43:21 -0800

    Adding Webview
    Web view now finished. Will research later

---
+++ WorldTrotter/WorldTrotter/WebViewController.swift
@@ -0,0 +1,26 @@
+//
+//  WebViewController.swift
+//  WorldTrotter
+//
+//  Created by Edrick Pascual on 2/29/16.
+//  Copyright © 2016 Edrick Pascual. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import WebKit
+
+
+class WebViewController: UIViewController  {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        print("WebViewController loaded")
+        
+    }
+    
+    
+}

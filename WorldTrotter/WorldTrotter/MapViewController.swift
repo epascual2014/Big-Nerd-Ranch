a4deb2b73af43a0afe225a99174ce0541e9d7059
Edrick Pascual <edrickpascual@Edricks-MacBook-Pro.local>
2016-02-29 20:17:53 -0800

    WorldTrotter segment buttons
    The Segmented buttons written in code is now clickable and the lay out
    correctly.

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -6,12 +6,13 @@
 //  Copyright © 2016 Edrick Pascual. All rights reserved.
 //
 
-import MapKit
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
     
     var mapView: MKMapView!
+    var currentTime: NSTimeZone!
     
     override func loadView() {
         
@@ -24,19 +25,42 @@ class MapViewController: UIViewController {
         let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
             segmentedControl.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
             segmentedControl.selectedSegmentIndex = 0
+            segmentedControl.addTarget(self,
+                action: "mapTypeChanged:",
+                forControlEvents: .ValueChanged)
+        
             segmentedControl.translatesAutoresizingMaskIntoConstraints = false
             view.addSubview(segmentedControl)
         
-        let topContstraint = segmentedControl.topAnchor.constraintEqualToAnchor(view.leadingAnchor)
-        let leadingConstraint = segmentedControl.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor)
-        let trailingConstraint = segmentedControl.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor)
-
-        topContstraint.active = true
+        let topConstraint = segmentedControl.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor,constant: 8)
+        
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor)
+        
+        topConstraint.active = true
         leadingConstraint.active = true
         trailingConstraint.active = true
     
+    
     }
     
+    func  mapTypeChanged(segControl: UISegmentedControl) {
+        
+        switch segControl.selectedSegmentIndex {
+            
+        case 0:
+            mapView.mapType = .Standard
+        case 1:
+            mapView.mapType = .Hybrid
+        case 2:
+            mapView.mapType = .Satellite
+        default:
+            break
+            
+        }
+        
+    }
     
     
     //var currentTime: NSTimeZone
@@ -44,6 +68,7 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+
         print("MVC loaded")
         
     }

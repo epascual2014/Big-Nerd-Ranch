2a5f48c8d2a2d336f84dcc58e511a6f07ec82a69
Edrick Pascual <edrickpascual@Edricks-MacBook-Pro.local>
2016-03-21 15:24:28 -0700

    Table View Created
    override functions created in ItemsController: numberOfRowsInSection
    and cellForRowAtIndexPath
    for the tableview

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -11,6 +11,26 @@ import UIKit
 
 class ItemsViewController: UITableViewController {
     
+    var itemStore: ItemStore!
     
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return itemStore.allItems.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        // Create an instance of UITableView Cell, with default appearance
+        let cell = UITableViewCell(style: .Value1, reuseIdentifier: "UITableViewCell")
+        
+        // Set the text on the cell with the description of the item that is at the nth index of items, 
+        // where n = row this cell will appear in on the tableview
+        let item = itemStore.allItems[indexPath.row]
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        
+        return cell
+        
+    }
     
 }
--- Homepwner/ItemStore.swift
+++ Homepwner/ItemStore.swift
@@ -11,5 +11,26 @@ import UIKit
 class ItemStore {
     
     var allItems = [Item]()
+ 
+    
+    func createItem() -> Item {
+        
+        let newItem = Item(random: true)
+        
+        // Added new items in the array
+        allItems.append(newItem)
+        
+        return newItem
+        
+    }
+    
+    init() {
+        
+        for _ in 0..<5 {
+            createItem()
+        }
+        
+    }
+    
     
 }

1fcf05139c74d1b7533f1aebbbd17f47eb6cd0ed
Edrick Pascual <edrickpascual@Edricks-MacBook-Pro.local>
2016-04-06 18:10:04 -0700

    NSKeyedArchiver and NSKeyedUnarchiver added
    Saving files in system so user can go to the home button and come back
    to the app. Item will be saved

--- Homepwner/ItemStore.swift
+++ Homepwner/ItemStore.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ItemStore {
     
+    // MARK: Constructing the URL File
     var allItems = [Item]()
- 
+    let itemArchiveURL: NSURL = {
+        let documentsDirectories = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        
+        let documentDirectory = documentsDirectories.first!
+        return documentDirectory.URLByAppendingPathComponent("items.archive")
+        
+    }()
     
+    init () {
+        
+        if let archivedItems = NSKeyedUnarchiver.unarchiveObjectWithFile(itemArchiveURL.path!) as? [Item] {
+            
+            allItems += archivedItems
+            
+        }
+        
+    }
+    
+    // MARK: Item created
     func createItem() -> Item {
         
         let newItem = Item(random: true)
@@ -24,6 +42,8 @@ class ItemStore {
         
     }
     
+    
+    // MARK: Item deleted
     func removeItem(item: Item) {
         
         if let index = allItems.indexOf(item) {
@@ -33,6 +53,8 @@ class ItemStore {
         
     }
     
+    
+    // MARK: Item (cell) moved
     func moveItemAtIndex(fromIndex: Int, toIndex: Int) {
         
         if fromIndex == toIndex {
@@ -48,6 +70,14 @@ class ItemStore {
         
         // Insert item in array at new location
         allItems.insert(movedItem, atIndex: toIndex)
+        
+    }
+    
+    
+    // MARK: Archive root Object
+    func saveChanges() -> Bool {
+        print("Saving items to: \(itemArchiveURL.path!)")
+        return NSKeyedArchiver.archiveRootObject(allItems, toFile: itemArchiveURL.path!)
         
     }
     

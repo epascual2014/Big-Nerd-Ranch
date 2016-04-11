3f12f13667b4ff7e03eab12565eea7755a56b47f
Edrick Pascual <edrickpascual@Edricks-MacBook-Pro.local>
2016-04-11 16:46:20 -0700

    TouchTracker

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,10 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Edrick Pascual on 4/11/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import CoreGraphics
+import Foundation

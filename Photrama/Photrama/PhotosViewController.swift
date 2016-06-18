cc4168cdbb9ead67dadadb39b491e4f283b75f1c
Edrick Pascual <edrickpascual@Edricks-MacBook-Pro.local>
2016-06-18 14:19:26 -0700

    Image View Created
    ImageView Created and Embedded

---
+++ Photrama/Photrama/PhotosViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosViewController.swift
+//  Photrama
+//
+//  Created by Edrick Pascual on 6/18/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
